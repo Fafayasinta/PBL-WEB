a2fe8d4778430872099b2d55c592e8bbbb6399ed
prihandiva <prihandiva1711@gmail.com>
2024-11-23 00:37:51 +0700

    Penambahan Seeder

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 //
 // Halaman utama
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 // Authentication routes
